a83a63fe08931a573f0c9a2a6fb494ccbfd03b12
Ram shasank Pathath <ramshasankpathath1@Rams-MacBook-Pro.local>
2015-07-12 12:17:16 -0500

    Initial Commit

---
+++ projinc2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  projinc2
+//
+//  Created by Ram shasank Pathath on 7/12/15.
+//  Copyright (c) 2015 Ram shasank Pathath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ projinc2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  projinc2
+//
+//  Created by Ram shasank Pathath on 7/12/15.
+//  Copyright (c) 2015 Ram shasank Pathath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
